2b17b8ff6e4802864e8271af051523d4ce77b1f5
canis <pavlouk2012@gmail.com>
2020-01-01 17:02:37 +0200

    kdjlas

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,8 +12,8 @@ import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import ServicesPage from 'containers/Services/Loadable';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
@@ -40,7 +40,7 @@ export default function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
+        <Route path="/services" component={ServicesPage} />
         <Route path="" component={NotFoundPage} />
       </Switch>
       <Footer />
---
+++ app/containers/ServicesPage/index.js
@@ -0,0 +1,52 @@
+/*
+ * FeaturePage
+ *
+ * List all the features
+ */
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+import H1 from 'components/H1';
+import List from './List';
+import ListItem from './ListItem';
+
+export default function FeaturePage() {
+  return (
+    <div>
+      <Helmet>
+        <title>Услуги</title>
+        <meta
+          name="description"
+          content="Feature page of React.js Boilerplate application"
+        />
+      </Helmet>
+      <H1>Hello world!</H1>
+      <List>
+        <ListItem>
+          <p>Hello world1</p>
+          <p>Hello world2</p>
+        </ListItem>
+
+        <ListItem>
+          <p>HelloWorld4</p>
+          <p>Hello World5</p>
+        </ListItem>
+
+        <ListItem>
+          <p>HelloWorld6</p>
+          <p>HelloWorld7</p>
+        </ListItem>
+
+        <ListItem>
+          <p>Helloworld8</p>
+          <p>HelloWorld9</p>
+        </ListItem>
+
+        <ListItem>
+          <p>HelloWorld10</p>
+          <p>HelloWorld11</p>
+        </ListItem>
+      </List>
+    </div>
+  );
+}
