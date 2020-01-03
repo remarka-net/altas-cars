280be26b8364d189486707a354c585345dfd43c5
canis <pavlouk2012@gmail.com>
2020-01-03 16:12:28 +0200

    vdjz

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -7,6 +7,7 @@
  */
 
 import React from 'react';
+import './index.css';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
