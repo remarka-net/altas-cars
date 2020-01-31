784772f0e7c67b4ea05ac4fd35f2f3a325f4347e
canis <pavlouk2012@gmail.com>
2020-01-31 10:38:59 +0200

    jkfsld

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -7,73 +7,70 @@
 import React from 'react';
 import './index.css';
 import main_pic from './main_pic.jpg';
-import MyForm from "../../components/Custom_form/index";
+import MyForm from '../../components/Custom_form/index';
 
 export default function Fruits() {
   return (
     <div className="main_wrapper">
       <div id="main_div">
         <div id="MainPart">
-            <br />
-            <h2 className="main_header">Автосервис ALTAS готов предложить вам полный спектр услуг</h2>
+          <br />
+          <h2 className="main_header">Автосервис ALTAS готов предложить вам полный спектр услуг</h2>
 
-              <div className="car_item1">
-                <span>Проверка и диагностика двигателя</span>
-              </div>
-              <div className="car_item1_extend">
-                Проверка ресурса движка, замена масла, заправка кондиционера.
-              </div>
+          <div className="car_item1">
+            <span>Проверка и диагностика двигателя</span>
+          </div>
+          <div className="car_item1_extend">
+            Проверка ресурса движка, замена масла, заправка кондиционера.
+          </div>
 
-              <div className="car_item2">
-                <span>Проверка электроники</span>
-              </div>
-              <div className="car_item2_extend">
-                Проверка системы авто на ошибки, замена проводки.
-              </div>
+          <div className="car_item2">
+            <span>Проверка электроники</span>
+          </div>
+          <div className="car_item2_extend">
+            Проверка системы авто на ошибки, замена проводки.
+          </div>
 
-              <div className="car_item3">
-                <span>Полная диагностика авто</span>
-              </div>
-              <div className="car_item3_extend">
-                Компьютерная диагностика технического состояния авто.
-              </div>
+          <div className="car_item3">
+            <span>Полная диагностика авто</span>
+          </div>
+          <div className="car_item3_extend">
+            Компьютерная диагностика технического состояния авто.
+          </div>
 
-              <div className="car_item4">
-                <span>Проверка на правильность пробега</span>
-              </div>
-              <div className="car_item4_extend">
-                Определяем реальный километраж авто.
-              </div>
+          <div className="car_item4">
+            <span>Проверка на правильность пробега</span>
+          </div>
+          <div className="car_item4_extend">
+            Определяем реальный километраж авто.
+          </div>
 
-              <div className="car_item5">
-                <span>Десятки других пунктов</span>
-              </div>
-              <div className="car_item5_extend">
-                Полировка, покраска, рихтовка дисков, вулканизация шин...
-              </div>
+          <div className="car_item5">
+            <span>Десятки других пунктов</span>
+          </div>
+          <div className="car_item5_extend">
+            Полировка, покраска, рихтовка дисков, вулканизация шин...
+          </div>
 
-              <div className="car_item6">
-                <span>Проверка на автоугон</span>
-              </div>
-              <div className="car_item6_extend">
-                Полная юридическая проверка авто при продаже, а также
-                сопровождение сделки.
-              </div>
+          <div className="car_item6">
+            <span>Проверка на автоугон</span>
+          </div>
+          <div className="car_item6_extend">
+            Полная юридическая проверка авто при продаже, а также сопровождение
+            сделки.
           </div>
         </div>
+      </div>
+      </div>
 
+      <div className="our_service">
 
-        <div className="our_service">
-
-        <h3 className="second_headline">Предлагаем для вас услуги планового ТО автомобиля</h3>
-
-     
+        <h3 className="second_headline">
+          Предлагаем для вас услуги планового ТО автомобиля
+        </h3>
 
         <MyForm />
-
-        </div>
-
+      </div>
     </div>
-    
   );
 }
