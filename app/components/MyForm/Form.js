245470168ac2d826c4c936a24fa5da3e24ad961e
canis <pavlouk2012@gmail.com>
2020-02-07 10:15:39 +0200

    jflk;sd

--- app/components/MyForm/Form.js
+++ app/components/MyForm/Form.js
@@ -3,32 +3,38 @@ import emailjs from 'emailjs-com';
 import './Form.css';
 
 export default function ContactUs() {
-
   function sendEmail(e) {
     e.preventDefault();
 
-    emailjs.sendForm('gmail', 'template_hsjTwTVo', e.target, 'user_gVUee7bxzx7Obe10GOfuU')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        'gmail',
+        'template_hsjTwTVo',
+        e.target,
+        'user_gVUee7bxzx7Obe10GOfuU',
+      )
+      .then(
+        result => {
           console.log(result.text);
           document.location.reload(true);
-      }, (error) => {
+        },
+        error => {
           console.log(error.text);
-      });
+        },
+      );
   }
   return (
-
     <form className="contact-form" onSubmit={sendEmail}>
-        <label>Предварительный заказ</label>
-        <hr/>
+      <label>Предварительный заказ</label>
+      <hr />
 
-      <input type="text" placeholder="Ваше ФИО*" name="user_name" />
+      <input required type="text" placeholder="Ваше ФИО*" name="user_name" />
 
-      <input type="numeric" placeholder="Номер телефона*" name="user_phone" />
+      <input required type="numeric" placeholder="Номер телефона*" name="user_phone" />
 
       <input type="email" placeholder="E-mail" name="user_email" />
 
       <input type="submit" value="Сделать заказ" />
     </form>
-
   );
 }
--- app/containers/ServicesPage/index.js
+++ app/containers/ServicesPage/index.js
@@ -1,7 +1,20 @@
+
+
+import './index.css';
+import img1 from "./img/motor.svg";
+import img2 from './img/transmision.svg';
+import img3 from './img/brake-disk.svg';
+import img4 from './img/battery.svg';
+import img5 from './img/shock-absorber.svg';
+import img6 from './img/wheel-and-manometer.svg';
+import img7 from './img/tachometer.svg';
+import img8 from './img/oil.svg';
+import car from './img/service_car.jpg';
+import pic1 from './img/car1.jpg';
 /*
- * FeaturePage
+ * 
  *
- * List all the features
+ * 
  */
 import React from 'react';
 import { Helmet } from 'react-helmet';
@@ -13,40 +26,45 @@ import ListItem from './ListItem';
 export default function FeaturePage() {
   return (
     <div>
-      <Helmet>
-        <title>Услуги</title>
-        <meta
-          name="description"
-          content="Feature page of React.js Boilerplate application"
-        />
-      </Helmet>
-      <H1>Наши услуги</H1>
-      <List>
-        <ListItem>
-          <p>Hello world1</p>
-          <p>Hello world2</p>
-        </ListItem>
+  <p class="subtitle fancy"><span>Наши услуги</span></p>
 
-        <ListItem>
-          <p>HelloWorld4</p>
-          <p>Hello World5</p>
-        </ListItem>
+  <h1 className="Services_headline">ТОВ "ALTAS" предлагает вам полный спектр услуг</h1>
 
-        <ListItem>
-          <p>HelloWorld6</p>
-          <p>HelloWorld7</p>
-        </ListItem>
+    <div className="car_repair">
+    <img src={img1} alt="motor"/>
+    <img src={img2} alt=""/>
+    <img src={img3} alt=""/>
+    </div>
+    <div className="main_car_repair">
+    <img src={img8} alt=""/>
+    <img className="service_car" src={car} alt=""/>
+    <img src={img4} alt=""/>
+    </div>
+    <div className="car_repair">
+      <img src={img7} alt=""/>
+      <img src={img6} alt=""/>
+      <img src={img5} alt=""/>
+    </div>
 
-        <ListItem>
-          <p>Helloworld8</p>
-          <p>HelloWorld9</p>
-        </ListItem>
+    <div className="service_order">
+    <img className="service_order_pic" src={pic1} alt=""/>
+    <div className="service_order_article">
+    <label>Компьютерная диагностика</label>
+    <p>
+    Чтобы ваше транспортное средство работало на протяжении
+     долгого времени, не нуждаясь при этом в дорогостоящем ремонте,
+    рекомендуется периодически проводить компьютерную диагностику 
+    авто. Благодаря этому специалисты смогут обнаружить
+    скрытые неполадки, которые в скором времени могут стать причиной 
+    серьезной поломки. 
+    </p>
+    </div>
+    </div>
 
-        <ListItem>
-          <p>HelloWorld10</p>
-          <p>HelloWorld11</p>
-        </ListItem>
-      </List>
+
+
+
+
     </div>
   );
 }
